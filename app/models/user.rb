415f6d09eb4f02325ae47c6653e01503b3a3008d
ikuma-e <139757329+ikuma-e@users.noreply.github.com>
2023-08-21 16:55:20 +0900

    ユーザー管理機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "には英数字を両方含む必要があります" }
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "には半角英数字を両方含む必要があります" }
 
   validates :nickname, presence: true
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :family_name_kana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/}
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :first_name_kana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/}
   validates :user_birth, presence: true
   
