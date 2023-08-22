0faefbec2f6f225a287bd19b1aeb16fd9db68a92
ikuma-e <139757329+ikuma-e@users.noreply.github.com>
2023-08-22 12:06:52 +0900

    商品の登録

---
+++ app/models/item.rb
@@ -0,0 +1,27 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  # ActiveHashとのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :delivery_address
+  belongs_to :delivery_price
+  belongs_to :delivery_time
+  belongs_to :state
+
+  validates :item_name, presence: true
+  validates :description, presence: true 
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ }, numericality: {greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}
+
+
+  # 以下カテゴリは---の時、保存できないようにする
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :state_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_price_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_address_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_time_id, numericality: { other_than: 1, message: "can't be blank" }
+
+
+
+end
