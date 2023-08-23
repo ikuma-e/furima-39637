2f31dee12f19b58a9b7af319d08cc9a41db0b3f2
ikuma-e <139757329+ikuma-e@users.noreply.github.com>
2023-08-23 18:47:10 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, except: [:index, :new, :create]
+  before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -22,10 +24,32 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
   private
 
   def item_params
     params.require(:item).permit(:item_name, :description, :price, :category_id, :state_id, :delivery_price_id,
                                  :delivery_address_id, :delivery_time_id, :image).merge(user_id: current_user.id)
   end
+
+  # 重複のためIdからデータを探す処理をまとめる
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  # ログインユーザーが出品者でない場合、強制的にトップページに移動させる
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
+  end
+
 end
